59baffba74da7458be4065f195c73ef9c92cacc0
Ari <ariannasg.ldn@gmail.com>
2020-05-20 10:33:53 +0100

    Updated hello-print

--- hello-print.py
+++ hello-print.py
@@ -4,4 +4,4 @@ x = 42
 
 print('Hello world using python 2 style (deprecated). %d' % x)
 print('Hello world using python 3 style. {}'.format(x))
-print(f'Hello world using the f function. {x}')
+print(f'Hello world using the f function. {x}. "f strings" are only available on python 3.6 onwards.')
