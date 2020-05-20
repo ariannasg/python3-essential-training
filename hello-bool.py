4985de7a2b76f37862ace0cd8d574af094d379b6
Ari <ariannasg.ldn@gmail.com>
2020-05-20 11:01:07 +0100

    Added script for trying the bool type

---
+++ hello-bool.py
@@ -0,0 +1,34 @@
+#! usr/bin/env python3
+
+# The bool type is a fundamental built-in type (all types are classes). Everything is an object in Python 3.
+
+x = True
+print(x)
+print('x is {}'.format(type(x)))
+x = 7 < 5
+print(x)
+print(type(x))
+
+x = None
+print(x)
+print('x is {}'.format(type(x)))
+if x:
+    print('True')
+else:
+    print('False')
+
+x = 0
+print(x)
+print('x is {}'.format(type(x)))
+if x:
+    print('True')
+else:
+    print('False')
+
+x = ""
+print(x)
+print('x is {}'.format(type(x)))
+if x:
+    print('True')
+else:
+    print('False')
