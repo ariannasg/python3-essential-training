e5919488c5e19837658a59cbfe6aa562d3b6cf6c
Ari <ariannasg.ldn@gmail.com>
2020-05-20 09:39:23 +0100

    Added script for using loops

---
+++ hello-loops.py
@@ -0,0 +1,14 @@
+#! usr/bin/env python3
+
+words = ['Hello', 'World', '!']
+print(f'The words array has {3} elements')
+
+print('Printing elements in words using a while loop:')
+n = 0
+while n < 3:
+    print(words[n])
+    n = n + 1
+
+print('Printing elements in words using a for loop:')
+for i in words:
+    print(i)
