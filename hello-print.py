c8f8a1bf2c46ac433fc073fa8fce48b67423fc73
Ari <ariannasg.ldn@gmail.com>
2020-05-20 09:25:39 +0100

    Added script for trying out different ways of printing and formatting a string

---
+++ hello-print.py
@@ -0,0 +1,7 @@
+#! usr/bin/env python3
+
+x = 42
+
+print('Hello world using python 2 style (deprecated). %d' % x)
+print('Hello world using python 3 style. {}'.format(x))
+print(f'Hello world using the f function. {x}')
