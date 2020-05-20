9b03e2e241f0b9e3d945dd818f1892c563d7d0ef
Ari <ariannasg.ldn@gmail.com>
2020-05-20 10:47:31 +0100

    Added script for trying out the string type

---
+++ hello-string.py
@@ -0,0 +1,53 @@
+#! usr/bin/env python3
+
+# Str is a fundamental built-in type (all types are classes). Everything is an object in Python 3.
+z = 'seven'
+print(z)
+print('z is {}'.format(type(z)))
+
+print('strings are objects so we can use their functions')
+print(z.upper())
+
+print('can have multi-line strings')
+z = """
+multi-line string
+"""
+print(z)
+
+print('can create spaces or 0s using the format function')
+z = 'seven {1} {0}'.format(8, 9)
+print(z)
+
+z = 'seven {1:>9} {0:>9}'.format(8, 9)
+print(z)  # seven         9         8
+
+z = 'seven {1:<9} {0:>9}'.format(8, 9)
+print(z)  # seven 9                 8
+
+z = 'seven "{1:<9}" "{0:>9}"'.format(8, 9)
+print(z)  # seven "9        " "        8"
+
+z = 'seven "{1:<09}" "{0:>09}"'.format(8, 9)
+print(z)  # seven "900000000" "000000008" -> only works with  0, a letter will throw error and numbers will add spaces
+
+za = 8
+zb = 9
+z = f'seven "{za:<09}" "{zb:>09}"'
+print(z)  # seven "900000000" "000000008"
+
+
+# seven
+# z is <class 'str'>
+# strings are objects
+# SEVEN
+# can have multi-line strings
+#
+# multi-line string
+#
+# can create spaces or 0s using the format function
+# seven 9 8
+# seven         9         8
+# seven 9                 8
+# seven "9        " "        8"
+# seven "900000000" "000000008"
+# seven "800000000" "000000009"
