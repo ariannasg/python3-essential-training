a63e7938bf86c05d605f6d9386c2d59c400a7508
Ari <ariannasg.ldn@gmail.com>
2020-05-20 13:59:27 +0100

    Added logic to hello-loops for trying a while loop using the console input

--- hello-loops.py
+++ hello-loops.py
@@ -13,6 +13,12 @@ print('Printing elements in words using a for loop:')
 for i in words:
     print(i)
 
+print('Trying a while loop using the console input')
+secret = 'swordfish'
+pw = ''
+while pw != secret:
+    pw = input("What's the secret word? ")
+
 # CONSOLE OUTPUT:
 # The words array has 3 elements
 # Printing elements in words using a while loop:
@@ -23,3 +29,7 @@ for i in words:
 # Hello
 # World
 # !
+# Trying a while loop using the console input
+# What's the secret word? dunno
+# What's the secret word? maybeispwd
+# What's the secret word? swordfish
