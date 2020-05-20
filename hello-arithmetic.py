1fa0dff6bbc306061a2c78c4313b97866ce01455
Ari <ariannasg.ldn@gmail.com>
2020-05-20 11:47:42 +0100

    Added script for trying arithmetic operators

---
+++ hello-arithmetic.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+
+x = 5
+y = 2
+
+print('Addition operator')
+z = x + y
+print(f'result is {z}')
+
+print('Subtraction operator')
+z = x - y
+print(f'result is {z}')
+
+print('Subtraction operator')
+z = x + y
+print(f'result is {z}')
+
+print('Division operator')
+z = x / y
+print(f'result is {z}')
+
+print('Integer division operator')
+z = x // y
+print(f'result is {z}')
+
+print('Remainder/Modulo operator')
+z = x % y
+print(f'result is {z}')
+
+print('Negative unary operator')
+z = -z
+print(f'result is {z}')
+
+print('Positive unary operator')
+z = +z
+print(f'result is {z}')
+
+# CONSOLE OUTPUT:
+# Addition operator
+# result is 7
+# Subtraction operator
+# result is 3
+# Subtraction operator
+# result is 7
+# Division operator
+# result is 2.5
+# Integer division operator
+# result is 2
+# Remainder/Modulo operator
+# result is 1
+# Negative unary operator
+# result is -1
+# Positive unary operator
+# result is -1
