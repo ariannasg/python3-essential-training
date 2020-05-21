5b45402b693805b3587acddec6a4e8c873882062
Ari <ariannasg.ldn@gmail.com>
2020-05-21 15:07:25 +0100

    Added logic to hello-version for trying the sys and os modules

--- hello-version.py
+++ hello-version.py
@@ -1,8 +1,16 @@
 #! usr/bin/env python3
 
 import platform
+import sys
+import os
 
-print('This is python version {}'.format(platform.python_version()))
+print('This is my python version: {}'.format(platform.python_version()))
+print('This is my sys platformL {}'.format(sys.platform))
+print('This is my os name: {}'.format(os.name))
+print('This is my working directory {}'.format(os.getcwd()))
 
 # CONSOLE OUTPUT:
 # This is python version 3.8.3
+# This is sys platform darwin
+# This is os name posix
+
