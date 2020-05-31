259b58fe469cf84be5f4d21c89dc2c12bc5399a9
Ari <ariannasg.ldn@gmail.com>
2020-05-31 17:08:11 +0100

    Using main method in truth value testing

--- advanced/truth_value_testing.py
+++ advanced/truth_value_testing.py
@@ -1,18 +1,23 @@
 #!usr/bin/env python3
 from decimal import Decimal
 
+
 # in python any object is considered to be True unless its class defines a
 # bool method that returns false or has a len method returning 0
+def main():
+    print(bool(False))
+    print(bool(0))
+    print(bool(0.0))
+    print(bool(0j))
+    print(bool(Decimal(0)))
+    print(bool([]))
+    print(bool({}))
+    print(bool(set()))
+    print(bool(range(0)))
 
-print(bool(False))
-print(bool(0))
-print(bool(0.0))
-print(bool(0j))
-print(bool(Decimal(0)))
-print(bool([]))
-print(bool({}))
-print(bool(set()))
-print(bool(range(0)))
+
+if __name__ == '__main__':
+    main()
 
 # CONSOLE OUTPUT:
 # False
